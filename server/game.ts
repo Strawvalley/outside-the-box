99856e175ae62a24456fc9fb1135794a54098714
NilsEngelbach <me@nilsengelbach.com>
2020-04-11 14:37:31 +0200

    Add models for game and user

---
+++ server/game.ts
@@ -0,0 +1,21 @@
+export class Game {
+  started: boolean;
+  admin: string;
+  state: 'not-started' | 'thinking' | 'guessing' | 'round-finished' | 'game-finished';
+
+  totalRounds: number;
+  round: number;
+  points: number;
+  secondsLeft: number;
+
+  constructor(admin: string) {
+    this.admin = admin;
+
+    this.started = false;
+    this.state = 'not-started';
+    this.totalRounds = 10;
+    this.round = 1;
+    this.points = 0;
+    this.secondsLeft = 30;
+  }
+}
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,14 @@
 import { server } from "./server";
 import { connection$, disconnect$, listenOnConnect } from "./connection";
+import { Game } from "./game";
 
 // Create HTTP server with "app" as handler
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`[INFO] Listening on port: ${port}`));
 
-const games = {};
+const games: {
+  [key: string]: Game
+} = {};
 
 connection$.subscribe(({ io, client }) => {
   console.log(`[CONNECTED] Client ${client.id}`);
@@ -25,8 +28,10 @@ disconnect$.subscribe(({ io, client }) => {
   const allUsersInRoom = getAllUsersInRoom(io, client.room);
 
   if (allUsersInRoom.length === 0) {
+    // If last player of room left, delete the game
     delete games[client.room];
   } else if (client.id === games[client.room].admin) {
+    // If the admin left the game -> assign new admin and update users for all clients
     games[client.room].admin = allUsersInRoom[0].id;
     allUsersInRoom[0].isAdmin = true;
     io.in(client.room).emit("all users in room update", allUsersInRoom);
@@ -37,32 +42,21 @@ disconnect$.subscribe(({ io, client }) => {
 listenOnConnect("room").subscribe(({ io, client, data }) => {
   console.log(`[INFO] Client ${client.id} joins room ${data.room} as ${data.username}`);
 
-  // Check if game already exists, if not -> create new game and set admin
-  if (!games[data.room]) {
-    games[data.room] = {
-      started: false,
-      admin: client.id,
-      activePlayer: undefined,
-      totalRounds: 10,
-      round: 1,
-      points: 0,
-      secondsLeft: 5,
-      state: "not-started" // 'not-started' | 'thinking' | 'guessing' | 'round-finished' | 'game-finished'
-    };
-  }
-
+  // Add user to the room
   const allSockets = io.sockets.sockets;
   allSockets[client.id].username = data.username;
   allSockets[client.id].room = data.room;
   client.join(data.room);
 
-  const allUsersInRoom = getAllUsersInRoom(io, data.room);
-
   // Send new user list to all users
+  const allUsersInRoom = getAllUsersInRoom(io, data.room);
   io.in(data.room).emit("all users in room update", allUsersInRoom);
 
+  // Check if game already exists, if not -> create new game and set admin
+  const game = games[data.room] ? games[data.room] : createNewGame(data.room, client.id);
+
   // Send current game state to new connect client
-  client.emit("update game state", { gameState: games[data.room] });
+  client.emit("update game state", { gameState: game });
 });
 
 listenOnConnect("initiate game").subscribe(({ io, client }) => {
@@ -79,14 +73,14 @@ listenOnConnect("initiate game").subscribe(({ io, client }) => {
   }
 });
 
-interface user {
+interface IUser {
   id: string,
   username: string,
   room: string,
   isAdmin: boolean
 }
 
-function getAllUsersInRoom(io, roomName: string): user[] {
+function getAllUsersInRoom(io, roomName: string): IUser[] {
   const allSockets = io.sockets.sockets;
   return Object.entries(allSockets)
     .map(([id, socket]: [any, any]) => ({
@@ -96,4 +90,10 @@ function getAllUsersInRoom(io, roomName: string): user[] {
       isAdmin: games[socket.room] ? id === games[socket.room].admin : false
     }))
     .filter(({ room }) => room === roomName);
+}
+
+function createNewGame(room: string, admin: string): Game {
+  const newGame = new Game(admin);
+  games[room] = newGame;
+  return newGame;
 }

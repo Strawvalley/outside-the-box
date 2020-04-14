e95548da99e5206f16a3db75f29e0bb1f48bf7d4
Engelbach <nils.engelbach@trumpf.com>
2020-04-14 14:46:19 +0200

    Add comments

--- server/managers/game_manager.ts
+++ server/managers/game_manager.ts
@@ -13,9 +13,16 @@ export class GameManager {
     this.games = {}
   }
 
+  /**
+   * Adds the user to the game, ensures the usernames are unique in a room.
+   * Returns a new username for the connect user, if a user with the same name is already connected to the game,
+   * otherwise it is treated as "reconnect" and the socketId and connection state of the user is updated.
+   * @returns the given username or newly created username if player with same username is already connected to the game
+   */
   public addUserToGame(gameId: string, userId: string, username: string): string {
     const game = this.games[gameId];
 
+    // Username does not exist in game yet
     if (game.users[username] === undefined) {
       game.users[username] = {
         socketId: userId,
@@ -24,17 +31,19 @@ export class GameManager {
       return username
     }
 
-    if (game.users[username].connected) {
+    // Username does already exist in game,
+    // check if it is a "reconnect" or create and return a new username for the connected user
+    if (!game.users[username].connected) {
+      game.users[username].socketId = userId;
+      game.users[username].connected = true
+      return username;
+    } else {
       const newUsername = this.getUnusedUsername(Object.keys(game.users), username);
       game.users[newUsername] = {
         socketId: userId,
         connected: true
       };
       return newUsername;
-    } else {
-      game.users[username].socketId = userId;
-      game.users[username].connected = true
-      return username;
     }
   }
 
@@ -66,6 +75,10 @@ export class GameManager {
     }
   }
 
+  /**
+   * Creates a new game if it does net exist yet, and adds the user to the game.
+   * @returns the username under which the user is connected to the game (no duplicated allowed)
+   */
   public createOrJoinGame(gameId: string, userId: string, username: string): string {
     if (!this.games[gameId]) {
       this.games[gameId] = new Game(userId, gameId, this.updateGameForAllUsers, this.updateGameForUser);

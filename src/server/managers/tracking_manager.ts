78d37c2d281090533625bf159a07802674b7d432
NilsEngelbach <me@nilsengelbach.com>
2020-04-18 11:46:02 +0200

    Update tracking to include parameters

--- src/server/managers/tracking_manager.ts
+++ src/server/managers/tracking_manager.ts
@@ -1,18 +1,21 @@
 import ua from 'universal-analytics'
 import { logWarning, logInfo } from './log_manager';
 
-const tracker = ua('UA-163934012-1', 'Server', { strictCidFormat: false }, { dh: 'https://outside-the-box.herokuapp.com/'});
+const trackingId = `Server${process.env.NODE_ENV === 'development' ? '-DEV' : ''}`;
+
+const tracker = ua(
+  'UA-163934012-1',
+  trackingId,
+  { strictCidFormat: false },
+  { dh: 'https://outside-the-box.herokuapp.com/'}
+);
 
 export function trackMetric(action: string, label: string, value: any): void {
-  if (process.env.NODE_ENV === 'development') {
-    logInfo(`[MOCK ONLY] Tracked Metric: ${action} - ${label} - ${value}`);
-  } else {
-    tracker.event("Server Metric", action, label, value, { value: value }, (err) => {
-      if (err) {
-        logWarning(`Could not track metric: ${err}`);
-      } else {
-        logInfo(`Tracked Metric: ${action} - ${label} - ${value}`);
-      }
-    });
-  }
+  tracker.event("Server Metric", action, label, value, { value: value }, (err) => {
+    if (err) {
+      logWarning(`[${trackingId}] Could not track metric: ${err}`);
+    } else {
+      logInfo(`[${trackingId}] Tracked Metric: ${action} - ${label} - ${value}`);
+    }
+  });
 }

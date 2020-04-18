5f9127979fb1b32864f7221337a1aefbe3ddb1c1
Phi Dang <dngphg@gmail.com>
2020-04-18 17:55:57 +0200

    Fix userlist

--- src/client/components/user-list.component.ts
+++ src/client/components/user-list.component.ts
@@ -7,7 +7,7 @@ export const UserList = Vue.extend({
     <p class="stats" style="text-align: left;"><b style="text-align: left;">{{ $t('userlistTitle') }}</b></p>
     <ul style="list-style: none;">
       <li v-for="user in Object.keys(users)" class="highlight">
-        {{ user === activePlayer ? '-->' : '' }} {{ user }} {{ users[user].connected ? '' : '$t('userlistDisconnected')' }} {{users[user].socketId === admin ? "$t('userlistAdmin')" : ""}}
+        {{ user === activePlayer ? '-->' : '' }} {{ user }} {{ users[user].connected ? '' : $t('userlistDisconnected') }} {{users[user].socketId === admin ? $t('userlistAdmin') : ""}}
       </li>
     </ul>
   </div>

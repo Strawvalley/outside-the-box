244e330254bedfbb7c54d8f01391c29c49412bc6
Phi Dang <dngphg@gmail.com>
2020-04-19 12:10:03 +0200

    Fix return type issue

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,9 +1,9 @@
-import Vue from 'vue';
+import Vue, { VNode } from 'vue';
 import App from './app.vue';
 import i18n from './plugins/i18n';
 
 new Vue({
   el: '#game',
   i18n,
-  render: h => h(App),
+  render: (h): VNode => h(App),
 })

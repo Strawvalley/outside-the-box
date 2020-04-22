dd6214b36d6f1cc76746d608e24a2387eb78d31d
Engelbach <nils.engelbach@trumpf.com>
2020-04-22 14:15:51 +0200

    Fix for ios

--- src/client/managers/audio-libs/jsfxr.ts
+++ src/client/managers/audio-libs/jsfxr.ts
@@ -434,11 +434,11 @@ var _sfxr_getAudioFn = function(wave) {
           var proc = actx.createBufferSource();
           proc.buffer = buff;
           proc.connect(actx.destination);
-          if ('AudioContext' in window) {
+          // if ('AudioContext' in window) {
             proc.start();
-          } else if ('webkitAudioContext' in window) {
-            proc.noteOn(0);
-          }
+          // } else if ('webkitAudioContext' in window) {
+          //   proc.noteOn(0);
+          // }
           this.channels.push(proc);
         }
       };
